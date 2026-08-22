22835e9b634ad32eb3ab11d798583b976901c513
M SRAVAN KUMAR VARMA <247r1a6690@cmrtc.ac.in>
2026-08-22 20:47:00 +0530

    Time: 22 ms (96.38%) | Memory: 103.9 MB (83.90%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int index = stack.pop();
+                ans[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
