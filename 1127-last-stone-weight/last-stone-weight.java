2b163427e20b5249091e81e8089bd6ff0af4000f
M SRAVAN KUMAR VARMA <247r1a6690@cmrtc.ac.in>
2026-08-28 22:28:07 +0530

    Time: 2 ms (31.70%) | Memory: 43.2 MB (15.11%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> b - a);
+        for(int stone :stones){
+            pq.add(stone);
+        }
+        while(pq.size() > 1){
+            int y = pq.poll();
+            int x = pq.poll();
+            if(x!=y){
+                pq.add(y-x);
+            }
+        }
+        return pq.isEmpty()?0:pq.poll();
+    }
+}
